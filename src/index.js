e92e10fde8139b91f1984c58d968ede0af99022e
Mat <mathew.teague@yewtreeweb.co.uk>
2020-04-10 13:04:50 +0100

    added

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
+import Tooltip from "./modules/tooltip";
+import Dropdown from "./modules/dropdown";
+import Tabs from "./modules/tabs";
 
+// create a tooltip
+const tooltip = new Tooltip(document.querySelector(".tooltip"));
+
+tooltip.init();
+
+// create dropdowns
+const dropdowns = document.querySelectorAll(".dropdown");
+
+dropdowns.forEach((dropdown) => {
+  const instance = new Dropdown(dropdown);
+  instance.init();
+});
+
+// create tabs
+const tabs = new Tabs(document.querySelector(".tabs"));
+tabs.init();
+
+console.log("test");
--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,83 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, 'dist/assets'),
-    filename: 'main.js',
+    path: path.resolve(__dirname, "dist/assets"),
+    filename: "main.js",
     chunkFilename: "[name].bundle.js",
   },
   devServer: {
     historyApiFallback: true,
-    contentBase: path.resolve(__dirname, 'dist'),
-    publicPath: '/assets/'
+    contentBase: path.resolve(__dirname, "dist"),
+    publicPath: "/assets/",
   },
   module: {
-    rules: [{
-      test: /\.js$/,
-      exclude: /node_modules/,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          cacheDirectory: true,
-          presets: ['@babel/preset-env'],
-          plugins: [
-            "@babel/plugin-syntax-dynamic-import"
-          ],
-        }
-      }
-    }]
-  }
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            cacheDirectory: true,
+            presets: ["@babel/preset-env"],
+            plugins: ["@babel/plugin-syntax-dynamic-import"],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.css$/,
+        exclude: /\.module\.css$/,
+        use: [
+          "style-loader",
+          "css-loader",
+          {
+            loader: "postcss-loader",
+            options: {
+              ident: "postcss",
+              config: {
+                path: path.resolve(__dirname, "postcss.config.js"),
+              },
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(sass|scss)$/,
+        exclude: /\.module\.(sass|scss)$/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              url: true,
+              import: true,
+              modules: false,
+            },
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              ident: "postcss",
+              config: {
+                path: path.resolve(__dirname, "postcss.config.js"),
+              },
+            },
+          },
+          "resolve-url-loader",
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
 };
